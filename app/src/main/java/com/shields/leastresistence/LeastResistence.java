a6a197514ca1bee1dee448b3a20b4679218e5c5c
Cory Shields <shieldscr88@gmail.com>
2016-05-08 12:05:36 -0400

    Tests for grid size completed

--- app/src/main/java/com/shields/leastresistence/LeastResistence.java
+++ app/src/main/java/com/shields/leastresistence/LeastResistence.java
@@ -1,11 +1,19 @@
 package com.shields.leastresistence;
 
 public class LeastResistence {
+    //rows columns
+    int[][] grid;
 
     public LeastResistence(int[][] grid) {
+        this.grid = grid;
     }
 
-    public boolean validateGrid() {
-        return true;
+    public boolean isGridValid() {
+        boolean isValid = true;
+
+        if((grid.length > 10) || (grid[0].length < 5) || (grid[0].length > 100))
+            isValid = false;
+
+        return isValid;
     }
 }
--- app/src/test/java/com/shields/leastresistence/LeastResistenceTest.java
+++ app/src/test/java/com/shields/leastresistence/LeastResistenceTest.java
@@ -11,6 +11,64 @@ public class LeastResistenceTest {
 
         LeastResistence leastResistence = new LeastResistence(grid);
 
-        assertTrue(leastResistence.validateGrid());
+        assertTrue(leastResistence.isGridValid());
+    }
+
+    @Test
+    public void validGridCannotBeLessThanFiveRows() {
+        int[][] grid = {{1, 2, 3}};
+
+        LeastResistence leastResistence = new LeastResistence(grid);
+
+        assertFalse(leastResistence.isGridValid());
+    }
+
+    @Test
+    public void validGridCannotBeLargerThanTenRows() {
+        int[][] invalidGrid = {{1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {9, 9, 9, 9, 9}};
+
+        LeastResistence leastResistence = new LeastResistence(invalidGrid);
+
+        assertFalse(leastResistence.isGridValid());
+    }
+
+    @Test
+    public void validGridCannotBeLargerThanOneHundredColumns() {
+        int[][] invalidGrid = buildGridOfSize(1, 101);
+
+        LeastResistence leastResistence = new LeastResistence(invalidGrid);
+
+        assertFalse(leastResistence.isGridValid());
+    }
+
+    @Test
+    public void validGridCanBeOneHundredColumns() {
+        int[][] invalidGrid = buildGridOfSize(1, 100);
+
+        LeastResistence leastResistence = new LeastResistence(invalidGrid);
+
+        assertTrue(leastResistence.isGridValid());
+    }
+
+    private int[][] buildGridOfSize(int rows, int columns) {
+        int[][] generatedArray = new int[rows][columns];
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                generatedArray[i][j] = 9;
+            }
+        }
+
+        return generatedArray;
     }
 }
