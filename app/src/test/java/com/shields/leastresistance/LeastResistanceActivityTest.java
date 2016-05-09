1dcb7ee5ad0fe50f325a4c546ee8b4361cabce2d
Cory Shields <shieldscr88@gmail.com>
2016-05-09 19:31:02 -0400

    Added flow button and text output

--- app/src/test/java/com/shields/leastresistance/LeastResistanceActivityTest.java
+++ app/src/test/java/com/shields/leastresistance/LeastResistanceActivityTest.java
@@ -1,7 +1,10 @@
 package com.shields.leastresistance;
 
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -13,10 +16,34 @@ import static org.junit.Assert.*;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class LeastResistanceActivityTest {
+
+    LeastResistanceActivity leastResistanceActivity;
+    EditText editTextButton;
+    Button runFlowButton;
+    TextView outputText;
+
+    @Before
+    public void setUp() {
+        leastResistanceActivity = Robolectric.setupActivity(LeastResistanceActivity.class);
+        editTextButton = (EditText)leastResistanceActivity.findViewById(R.id.row_input);
+        runFlowButton = (Button)leastResistanceActivity.findViewById(R.id.run_flow_button);
+        outputText = (TextView)leastResistanceActivity.findViewById(R.id.text_output);
+    }
+
     @Test
     public void leastResistanceActivityContainsRowInput() {
-        LeastResistanceActivity activity = Robolectric.setupActivity(LeastResistanceActivity.class);
-        View editTextFragment = activity.findViewById(R.id.row_input);
-        assertNotNull(editTextFragment);
+        assertNotNull(editTextButton);
+    }
+
+    @Test
+    public void leastResistanceActivityContainsRunFlowButton() {
+        assertNotNull(runFlowButton);
+        assertEquals(runFlowButton.getText(), "Run Flow");
+    }
+
+    @Test
+    public void leastResistanceActivityContainsTextOutput() {
+        assertNotNull(outputText);
+        assertEquals(outputText.getText(), "");
     }
 }
